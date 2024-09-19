b60f289caccf689693b1eb88131a09442b9573a0
wind_nr <66172432+adfio1234@users.noreply.github.com>
2024-09-19 09:02:20 +0900

    [level 1] Title: 달리기 경주, Time: 157.72 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -1,18 +1,19 @@
 function solution(players, callings) {
-    var answer = [];
-    var prize={};
-    let tmp="";
-    for(let i=0;i<players.length;i++)
-        {
-            prize[players[i]]=i;
-        }
-    for(let i=0;i<callings.length;i++)
-        {
-            prize[callings[i]]--;//순위
-            prize[players[prize[callings[i]]]]++;//이전사람
-            tmp=players[prize[callings[i]]];
-            players[prize[callings[i]]]=players[prize[callings[i]]+1];
-            players[prize[callings[i]]+1]=tmp;
-        }
+    let idx;
+    let name1;
+    let name2;
+    const idxList = {}
+
+    players.forEach((name,index)=>idxList[name]=index)
+    for(let call of callings){
+        idx = idxList[call]
+        name1 = players[idx]
+        name2 = players[idx-1]
+        idxList[call]-=1
+        idxList[name2]+=1
+        players[idx] = name2
+        players[idx-1] = name1
+    }    
+
     return players;
 }
