902f28f1739e0719e3fab319c0f4bdb56ae2e6e6
wind_nr <66172432+adfio1234@users.noreply.github.com>
2025-07-02 18:02:39 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 0.08 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string number) {
+    int answer = 0;
+    for(int i=0;i<number.length();i++)
+    {
+        answer+=(int)number[i]-48;
+    }
+    return answer%9;
+}
