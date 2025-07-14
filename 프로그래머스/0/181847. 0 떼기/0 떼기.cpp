05b4f7a3a42b9ffa996c3b37e54707f8d92c7bb7
wind_nr <66172432+adfio1234@users.noreply.github.com>
2025-07-14 18:40:02 +0900

    [level 0] Title: 0 떼기, Time: 0.02 ms, Memory: 3.74 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.cpp
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.cpp
@@ -4,12 +4,14 @@
 using namespace std;
 
 string solution(string n_str) {
-    if(n_str[0]=='0')
-    {
-        while(n_str[0]=='0')
-        {
-            n_str=n_str.substr(1);
-        }
-    }
-    return n_str;
+    // if(n_str[0]=='0')
+    // {
+    //     while(n_str[0]=='0')
+    //     {
+    //         n_str=n_str.substr(1);
+    //     }
+    // }
+    return to_string(stoi(n_str));
 }
+
+//string을 stoi로변환후 to_string으로 변환하면 더 쉽게풀수있다.
