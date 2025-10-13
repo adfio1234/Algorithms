b0a1fa542f0b74ee944328fb31fe2b7ad4866d67
wind_nr <66172432+adfio1234@users.noreply.github.com>
2025-10-13 19:47:03 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 1.92 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
@@ -1,15 +1,6 @@
 def solution(my_string, queries):
-    original=[]
+    my_string=list(my_string)
     for i,j in queries:
-        original.append(my_string[:i])
-        original.append(my_string[j+1:])
+        my_string[i:j+1]=my_string[i:j+1][::-1]
 
-        piece=my_string[i:j+1]
-        reverse_slice=piece[::-1]
-        
-        my_string=""
-        my_string=original[0]+reverse_slice+original[1];
-        
-        original=[]
-
-    return my_string
+    return ''.join(my_string)
