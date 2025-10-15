d2145beee7fe236a8b69a39536141442cad920ae
wind_nr <66172432+adfio1234@users.noreply.github.com>
2025-10-15 12:33:17 +0900

    [level 0] Title: 커피 심부름, Time: 0.11 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
@@ -1,9 +1,10 @@
 def solution(order):
-    answer = 0
-    for i in order:
-        if i.find('cafelatte')!=-1:
-            answer+=5000
-        else:
-            answer+=4500
+#     answer = 0
+#     for i in order:
+#         if i.find('cafelatte')!=-1:
+#             answer+=5000
+#         else:
+#             answer+=4500
         
-    return answer
+#     return answer
+    return sum([5000 if 'cafelatte' in i else 4500 for i in order])
