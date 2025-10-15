6813a97a351a700efdb26d0b56205047b1a769e4
wind_nr <66172432+adfio1234@users.noreply.github.com>
2025-10-15 13:45:04 +0900

    [level 0] Title: qr code, Time: 0.00 ms, Memory: 9.33 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.py
+++ 프로그래머스/0/181903. qr code/qr code.py
@@ -1,6 +1,9 @@
 def solution(q, r, code):
-    answer = ''
-    for i in range(len(code)):
-        if i%q==r:
-            answer+=code[i]
-    return answer
+    # answer = ''
+    # for i in range(len(code)):
+    #     if i%q==r:
+    #         answer+=code[i]
+    # return answer
+    # return ''.join([code[i] for i in range(len(code)) if i%q==r])
+      return code[r::q]
+    #결국 r부터시작해서 q의간격으로 만드는문제
