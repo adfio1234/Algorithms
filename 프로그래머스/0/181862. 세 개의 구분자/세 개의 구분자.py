0c32defd98ea36b7c8a22baec71db42fbf82731e
wind_nr <66172432+adfio1234@users.noreply.github.com>
2025-10-15 14:42:41 +0900

    [level 0] Title: 세 개의 구분자, Time: 9.52 ms, Memory: 22.3 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
@@ -1,13 +1,15 @@
 def solution(myStr):
-    answer=[]
-    tmp=''
-    start=0
-    for i in range(len(myStr)):
-        if myStr[i]=='a' or myStr[i]=='b' or myStr[i]=='c':
-            answer.append(myStr[start:i])
-            start=i+1
-    answer.append(myStr[start:])
-    answer=[i for i in answer if i]
-    if len(answer)==0:
-        answer.append("EMPTY")
-    return answer
+#     answer=[]
+#     tmp=''
+#     start=0
+#     for i in range(len(myStr)):
+#         if myStr[i]=='a' or myStr[i]=='b' or myStr[i]=='c':
+#             answer.append(myStr[start:i])
+#             start=i+1
+#     answer.append(myStr[start:])
+#     answer=[i for i in answer if i]
+#     if len(answer)==0:
+#         answer.append("EMPTY")
+#     return answer
+    myStr=myStr.replace('a',' ').replace('b',' ').replace('c',' ').split() 
+    return myStr if myStr else ["EMPTY"]
