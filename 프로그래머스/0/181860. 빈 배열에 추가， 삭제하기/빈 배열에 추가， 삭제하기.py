f66c8bb5832faf4f7c84a68b64df7c953000a603
wind_nr <66172432+adfio1234@users.noreply.github.com>
2025-10-15 14:58:02 +0900

    [level 0] Title: 빈 배열에 추가, 삭제하기, Time: 0.11 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.py
+++ 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.py
@@ -2,10 +2,10 @@ def solution(arr, flag):
     answer = []
     for i,j in enumerate(flag):
         if j:
-            for k in range(arr[i]*2):
-                answer.append(arr[i])
+            answer+=[arr[i]]*arr[i]*2
         else:
-            for k in range(arr[i]):
-                answer.pop()
+            # for k in range(arr[i]):
+            #     answer.pop()
+            answer=answer[:-arr[i]]
                 
     return answer
