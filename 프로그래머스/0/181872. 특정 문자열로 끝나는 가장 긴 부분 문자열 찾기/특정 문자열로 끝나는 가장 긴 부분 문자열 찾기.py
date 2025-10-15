7461b866d0b56c3f0c64c706ccb7c21c0ad6614d
wind_nr <66172432+adfio1234@users.noreply.github.com>
2025-10-15 15:24:14 +0900

    [level 0] Title: 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기, Time: 0.00 ms, Memory: 9.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.py
@@ -0,0 +1,6 @@
+def solution(myString, pat):
+    reverse_myString = myString[::-1]
+    pat=pat[::-1]
+    
+    index=reverse_myString.find(pat)
+    return myString[:-index] if myString[:-index] else myString
