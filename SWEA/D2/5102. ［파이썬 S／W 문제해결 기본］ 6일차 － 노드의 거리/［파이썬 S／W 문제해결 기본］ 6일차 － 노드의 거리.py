a3492f0a68fb6a1621e199078936d6fc02443d90
wind_nr <66172432+adfio1234@users.noreply.github.com>
2025-10-31 13:54:53 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 6일차 - 노드의 거리, Time: 83 ms, Memory: 53,888 KB -BaekjoonHub

--- SWEA/D2/5102. ［파이썬 S／W 문제해결 기본］ 6일차 － 노드의 거리/［파이썬 S／W 문제해결 기본］ 6일차 － 노드의 거리.py
+++ SWEA/D2/5102. ［파이썬 S／W 문제해결 기본］ 6일차 － 노드의 거리/［파이썬 S／W 문제해결 기본］ 6일차 － 노드의 거리.py
@@ -1,10 +1,11 @@
+#bfs풀이
 from collections import deque
 
 def bfs(arr,start,end):
     visited=set()
     queue=deque([(start,0)])
     distance=0
-    
+
     while queue:
         node,dist=queue.popleft()
         if node==end:
@@ -14,13 +15,46 @@ def bfs(arr,start,end):
                 visited.add(i)
                 queue.append((i,dist+1))
     return 0
+
+#dfs 풀이
+
+# def leastDistance(arr,start,end):
+#     visited=[False]*len(arr)
+#     min=100
+#
+#     def dfs(arr,start,end,dist):
+#         nonlocal min
+#         if dist>min:
+#             return
+#         if start == end:
+#             min= min if min<dist else dist
+#
+#         for i in range(1,len(arr)):
+#             if not visited[i-1] and arr[start][i]==1:
+#                 visited[i-1]=True
+#                 dfs(arr,i,end,dist+1)
+#                 visited[i-1]=False
+#
+#     dfs(arr,start,end,0)
+#
+#     if min==100:
+#         return 0
+#     else:
+#         return min
+
 T=int(input())
+
 for t in range(T):
+
     V,E=map(int,input().split())
     arr=[[0]*(V+1) for _ in range(V+1)]
+
     for i in range(E):
         x,y=map(int,input().split())
         arr[x][y]=1
         arr[y][x]=1
+
     start,end=map(int,input().split())
+
     print(f"#{t+1} {bfs(arr,start,end)}")
+    #print(f"#{t+1} {leastDistance(arr,start,end)}")
