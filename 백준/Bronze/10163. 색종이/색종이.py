ee76beb6235af74c2808afad7b62811ae9917589
wind_nr <66172432+adfio1234@users.noreply.github.com>
2025-11-20 18:15:04 +0900

    [Bronze I] Title: 색종이, Time: 268 ms, Memory: 39196 KB, Score: 53 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,16 +1,16 @@
-N=int(input())
-arr=[[0]*101 for _ in range(101)]
-ans=[0]*(N+1)
-for i in range(1,N+1):
-    x1,y1,x2,y2=map(int,input().split())
-    x2+=x1
-    y2+=y1
-    for j in range(x1,x2):
-        for k in range(y1,y2):
-            arr[j][k]=i
-for i in range(len(arr)):
-    for j in range(len(arr[i])):
-        ans[arr[i][j]]+=1
+N = int(input())
+arr = [[0]*1001 for _ in range(1001)]
+ans = [0]*(N+1)
 
-for i in range(1,len(ans)):
+for paper_num in range(1, N+1):
+    x, y, w, h = map(int, input().split())
+    for i in range(y, y+h):
+        for j in range(x, x+w):
+            arr[i][j] = paper_num
+
+for i in range(1001):
+    for j in range(1001):
+        ans[arr[i][j]] += 1
+
+for i in range(1, N+1):
     print(ans[i])
