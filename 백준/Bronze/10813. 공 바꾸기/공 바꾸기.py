0ea86bd5d70c48673b92a4d5213a9129475092aa
wind_nr <66172432+adfio1234@users.noreply.github.com>
2025-12-13 23:54:36 +0900

    [Bronze II] Title: 공 바꾸기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -0,0 +1,7 @@
+N,M=map(int,input().split())
+arr=[i for i in range(1,N+1)]
+for i in range(M):
+    i,j=map(int,input().split())
+    arr[i-1],arr[j-1]=arr[j-1],arr[i-1]
+for i in arr:
+    print(i,end=" ")
