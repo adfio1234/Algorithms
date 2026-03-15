8a9777b663f07c1a4cd950deb625bd7c74766039
wind_nr <66172432+adfio1234@users.noreply.github.com>
2026-03-15 22:44:42 +0900

    [Gold III] Title: 별자리 만들기, Time: 192 ms, Memory: 18804 KB -BaekjoonHub

--- 백준/Gold/4386. 별자리 만들기/별자리 만들기.java
+++ 백준/Gold/4386. 별자리 만들기/별자리 만들기.java
@@ -1,5 +1,4 @@
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,68 +8,48 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	static class Star{
-		private double x;
-		private double y;
-		
-		Star(double x,double y){
-			this.x=x;
-			this.y=y;
-		}
-
-
-	}
+	static public ArrayList<double []> starList=new ArrayList<>();
 	
-	static final double INF=Double.MAX_VALUE;
-	
-	static ArrayList<Star> starList=new ArrayList<>();
 	static double getDistance(int idx,int target) {
-		Star start=starList.get(idx);
-		Star end=starList.get(target);
-		
-		double nx=Math.abs(start.x-end.x);
-		double ny=Math.abs(start.y-end.y);
+		double[] start=starList.get(idx);
+		double[] end=starList.get(target);
+		double nx=Math.abs(start[0]-end[0]);
+		double ny=Math.abs(start[1]-end[1]);
 		
 		return Math.sqrt(nx*nx+ny*ny);
 	}
 	
 	static double prim(int size) {
-		
 		boolean[] visited=new boolean[size];
 		
-		PriorityQueue<double[]> pq=new PriorityQueue<>((a,b)->Double.compare(a[1], b[1]));
-		
-		Star start=starList.get(0);
-		pq.add(new double[] {0.0,0.0});
-		
-		double zodiac=0;
-		int connected=0;
-		while(!pq.isEmpty()) {
-			double[] cur=pq.poll();
-			int idx=(int) cur[0];
-			
-			Star curStar=starList.get(idx);
-			double weight=cur[1];
-			
-			if(visited[idx])continue;
-			
-			visited[idx]=true;
-			connected++;
-			zodiac+=weight;
-			
-			if(connected==size)break;
-			
-			for(int i=0;i<size;i++) {
-				if(i==idx)continue;
-				if(visited[i])continue;
-				pq.add(new double[] {i,getDistance(idx, i)});
-			}
-			
-			
-		}
-		return zodiac;
-		
-		
+		PriorityQueue<double []>pq=new PriorityQueue<>((a,b)->Double.compare(a[1], b[1]));
+		 pq.add(new double[] {0.0,0.0});
+		 
+		 double zodiac=0;
+		 int connected=0;
+		 
+		 while(!pq.isEmpty()) {
+			 double[] cur= pq.poll();
+			 int idx=(int)cur[0];
+			 
+			 double weight=cur[1];
+			 
+			 if(visited[idx])continue;
+			 
+			 visited[idx]=true;
+			 connected++;
+			 zodiac+=weight;
+			 
+			 if(connected==size)break;
+			 
+			 for(int i=0;i<size;i++) {
+				 if(i==idx)continue;
+				 if(visited[i])continue;
+				 pq.add(new double[] {i,getDistance(idx, i)});
+			 }
+		 }
+		 
+		 return zodiac;
 	}
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -83,7 +62,7 @@ public class Main {
 			st=new StringTokenizer(br.readLine());
 			double x=Double.parseDouble(st.nextToken());
 			double y=Double.parseDouble(st.nextToken());
-			starList.add(new Star(x,y));
+			starList.add(new double[] {x,y});
 		}
 		
 		System.out.printf("%.2f",prim(starNum));
