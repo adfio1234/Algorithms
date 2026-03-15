bce18f7040ebffe694873a0cc275a0a9c3141d2a
wind_nr <66172432+adfio1234@users.noreply.github.com>
2026-03-15 22:14:00 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 616 ms, Memory: 55056 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -3,62 +3,35 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static class Edge{
-		private int target;
-		private int weight;
-		
-		Edge(int target,int weight){
-			this.target=target;
-			this.weight=weight;
-		}
-
-
+	static int[] parent;
+	static ArrayList<int[]> edgeList=new ArrayList<>();
+	
+	static int[] makeSet(int vertexNum) {
+		int[] set=new int[vertexNum];
+		for(int i=0;i<vertexNum;i++)set[i]=i;
+		return set;
 	}
 	
-	static final int INF=Integer.MAX_VALUE;
-	
-	static boolean[] visited;
-	static ArrayList<Edge>[] edgeList;
-	
-	
-	static int prim(int vertex) {
+	static boolean unionSet(int start,int end) {
+		int parent1=findSet(start);
+		int parent2=findSet(end);
 		
-		//정렬및 거리 초기화
-		int[] minDist=new int[vertex];
-		for(int i=0;i<vertex;i++)minDist[i]=INF;
-
-	
-		//0부터 시작
-		minDist[0]=0;
-		
-		int distance=0;
+		if(parent1==parent2)return false;
+		parent[parent1]=parent2;
+		return true;
 		
 		
-		for(int i=0;i<vertex;i++) {
-			int cur=-1;
-			int min=INF;
-			
-			for(int j=0;j<vertex;j++) {
-				if(!visited[j]&&minDist[j]<min) {
-					cur=j;
-					min=minDist[j];
-				}
-			}
-			visited[cur]=true;
-			distance+=min;
-			
-			for(Edge j:edgeList[cur]) {
-				if(!visited[j.target]&&minDist[j.target]>j.weight) {
-					minDist[j.target]=j.weight;
-				}
-			}
-		}
-		return distance;
+	}
+	
+	static int findSet(int element) {
+		if(element==parent[element])return element;
+		else return parent[element]=findSet(parent[element]);
 	}
 	
 	public static void main(String[] args) throws IOException {
@@ -69,22 +42,31 @@ public class Main {
 		int vertexNum=Integer.parseInt(st.nextToken());
 		int edgeNum=Integer.parseInt(st.nextToken());
 		
-		visited=new boolean[vertexNum];
-		edgeList=new ArrayList[vertexNum];
+		int result=0;
 		
-		for(int i=0;i<vertexNum;i++)edgeList[i]=new ArrayList<>();
+		parent=makeSet(vertexNum);
+		
 		
 		for(int i=0;i<edgeNum;i++) {
 			st=new StringTokenizer(br.readLine());
+			
 			int start=Integer.parseInt(st.nextToken())-1;
 			int end=Integer.parseInt(st.nextToken())-1;
 			int weight=Integer.parseInt(st.nextToken());
-			
-			edgeList[start].add(new Edge(end,weight));
-			edgeList[end].add(new Edge(start,weight));
+			edgeList.add(new int[] {start,end,weight});
 		}
 		
-		System.out.println(prim(vertexNum));
+		Collections.sort(edgeList,(a,b)->a[2]-b[2]);
+		for(int[] edge:edgeList) {
+			int start=edge[0];
+			int end=edge[1];
+			int weight=edge[2];
+			if(unionSet(start,end))result+=weight;
+		}
+		
+		
+		System.out.println(result);
+		
 	}
 
 }
